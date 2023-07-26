1608870d2d0ee7f76d6774249024dea1d5b7dcd2
root <root@DESKTOP-SF230JF>
2023-07-26 21:34:56 +0200

    Add formatter config

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,5 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
+import model.Board
 
-def msg = "I was compiled by Scala 3. :)"
+@main def hello: Unit =
+  val board = Board()
+  println(board)

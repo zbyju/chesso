daba67a6d19b4cbc0f28bd032eeb0a21d35b3b97
zbyju <zbynek.jurica@gmail.com>
2024-10-08 00:26:04 +0200

    Improve BitBoard with more utility methods

--- src/main/scala/model/BitBoard.scala
+++ src/main/scala/model/BitBoard.scala
@@ -2,20 +2,74 @@ package model
 
 import model.pieces.CharPiece
 
-final case class BitBoard private (board: BitArray) {
+final case class BitBoard private (board: BitArray) extends BitBoardLike {
+
+  // ***************************** SET ************************************
+
   def set(pos: Coords): BitBoard = this.set(BitPosition.fromCoords(pos))
   def set(pos: BitPosition): BitBoard = BitBoard(this.board.set(pos))
+
+  // **************************** RESET ***********************************
 
   def reset(pos: Coords): BitBoard = this.reset(BitPosition.fromCoords(pos))
   def reset(pos: BitPosition): BitBoard = BitBoard(this.board.reset(pos))
 
+  // ***************************** GET ************************************
+
   def get(pos: Coords): Boolean = this.get(BitPosition.fromCoords(pos))
   def get(pos: BitPosition): Boolean = this.board.get(pos)
+
+  override def getPositions: Seq[Coords] =
+    this.board.positionsOfSetBits().map(_.toCoords())
+
+  // ***************************** CHECK **********************************
+
+  override def isOccupied(position: BitPosition): Boolean = this.get(position)
+
+  // **************************** COUNT ***********************************
+
+  override def countPieces: Int = board.countSetBits()
+
+  // ****************************** REST **********************************
+
+  def combine(that: BitBoard): BitBoard =
+    BitBoard.fromLong(this.board.toLong | that.board.toLong)
+
+  def invert: BitBoard = BitBoard(this.board.invert())
+
+  override def toString(): String = {
+    val sb = new StringBuilder
+    for (rank <- 7 to 0 by -1) {
+      sb.append(s"${rank + 1}|")
+      for (file <- 0 to 7) {
+        val pos = BitPosition.fromCoords(Coords(rank.toByte, file.toByte).get)
+        val bit = if (board.get(pos)) "1" else "0"
+        sb.append(s"$bit ")
+      }
+      sb.append("\n")
+    }
+    sb.append("-----------------\n")
+    sb.append(" |a b c d e f g h\n")
+    sb.toString
+  }
 }
 
 object BitBoard {
+  def empty = BitBoard.fromLong(0)
+
   def apply(positions: Coords*): BitBoard = {
     val ps = positions.map(p => BitPosition.fromCoords(p))
     BitBoard(BitArray(ps*))
   }
+
+  def fromLong(value: Long): BitBoard = {
+    val array = BitArray.fromLong(value)
+    BitBoard(array)
+  }
+
+  def combine(b1: BitBoard, b2: BitBoard): BitBoard =
+    BitBoard.fromLong(b1.board.toLong | b2.board.toLong)
+
+  def combineBoards(boards: Seq[BitBoard]): BitBoard =
+    boards.reduceLeft((acc, board) => acc.combine(board))
 }
---
+++ src/main/scala/model/BitBoardLike.scala
@@ -0,0 +1,33 @@
+package model
+
+trait BitBoardLike {
+
+  // Check if a given position is occupied (bit set to 1)
+  def isOccupied(position: BitPosition): Boolean
+  def isOccupied(coords: Coords): Boolean = isOccupied(
+    BitPosition.fromCoords(coords)
+  )
+
+  // Check if mulitple positions are occupied (bits set to 1)
+  def allOccupied(positions: Seq[Coords]): Boolean =
+    positions.forall(isOccupied)
+
+  // Check if any position is occupied
+  def anyOccupied(positions: Seq[Coords]): Boolean =
+    positions.exists(isOccupied)
+
+  // Check if all positions are empty
+  def checkEmpty(positions: Seq[Coords]): Boolean =
+    positions.forall(isEmpty)
+
+  // Get coordinates of all positions where the bit is set (1s in the bitboard)
+  def getPositions: Seq[Coords]
+
+  // Check if the bitboard is completely empty (no bits set to 1)
+  def isEmpty(position: BitPosition): Boolean = !isOccupied(position)
+  def isEmpty(coords: Coords): Boolean = !isOccupied(coords)
+
+  // Get the count of pieces (number of 1s in the bitboard)
+  def countPieces: Int
+
+}
